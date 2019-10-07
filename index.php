1286ab1df0a7031b23d15b3e265e8871df542630
Jenkins Automation Server <jenkins@ip-172-31-25-103.eu-west-2.compute.internal>
2019-10-08 00:34:49 +0300

    add config beanstalk

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>===============111111========================Congratulations!</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
+        <h1>==111111==Congratulations!</h1>
+        <p>1111 Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <p>1111 You are running PHP version <?= phpversion() ?></p>
     </section>
 
     <section class="instructions">
